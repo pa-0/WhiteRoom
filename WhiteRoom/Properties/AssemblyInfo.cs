27df1cbb1c6739124d37a119f305b9feabb8e144
Joe DF <joedf@live.ca>
2015-03-25 14:40:00 -0400

    Update About Info

--- WhiteRoom/Properties/AssemblyInfo.cs
+++ WhiteRoom/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WhiteRoom")]
-[assembly: AssemblyDescription("WhiteRoom is a full screen, distraction free, writing environment. WhiteRoom is a fork, a modernized, maintained, version of DarkRoom (or Dark Room W). Unlike standard word processors that focus on features, Dark Room W is just about you and your text. Basically, Dark Room W is a clone of the original WriteRoom that is an OS X exclusive application.")]
+[assembly: AssemblyDescription("WhiteRoom is a full screen, distraction free, writing environment. WhiteRoom is a fork, a modernized, maintained, version of DarkRoom (or Dark Room W). Unlike standard word processors that focus on features, DarkRoom is just about you and your text. Basically, DarkRoom is a clone of the original WriteRoom that is an OS X exclusive application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WhiteRoom")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d480df88-eea0-446d-bbed-fb6d614346da")]
